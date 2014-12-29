735f6aff51b9b503c40233f9c1545b314296c027
Mausy5043 <mausy5043@users.noreply.github.com>
2014-12-29 14:20:33 +0100

    New script `18-get-uptime.py`

---
+++ 18-get-uptime.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python
+
+import sys, commands, time
+
+def main():
+    # Everybody! Remember where we parked! -Capt. Kirk
+    orig_stdout = sys.stdout
+
+    # Define output file
+    f = file('/tmp/18-uptime.txt', 'a')
+    # Redefine system output to our file
+    sys.stdout = f
+
+    # Get the time and date in human-readable form...
+    outDate = commands.getoutput("date '+%F %H:%M:%S'")
+    # ... and machine-readable form (UNIX-epoch)
+    outUxDate = commands.getoutput("date +%s")
+
+
+    uptime = commands.getoutput("cat /proc/uptime").split()
+
+
+    # Print the data
+    print '{0}, {1}, {2}, {3}'.format(outDate, outUxDate, uptime[1], uptime[2])
+
+    # Close the file
+    f.close()
+
+    # Re-set the stdout
+    sys.stdout = orig_stdout
+
+if __name__ == "__main__":
+    main()
