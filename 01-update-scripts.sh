ca683eafaa6cb28dc1501bbe025e5a5d83bc6aa2
Mausy5043 <mausy5043@users.noreply.github.com>
2014-12-29 19:01:00 +0100

    Retrieve contents of `~/bin` from local server

--- 01-update-scripts.sh
+++ 01-update-scripts.sh
@@ -14,6 +14,7 @@ if [ $minute -eq "42" ]; then
   if [ ! -e ~/.rsyncd.secret ]; then
     sudo mount /mnt/backup
     cp /mnt/backup/rbmain/.rsyncd.secret ~/.rsyncd.secret
+    cp /mnt/backup/rbmain/bin/* ~/bin/
     sudo umount /mnt/backup
   fi
 fi
