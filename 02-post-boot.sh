ddef85fa47b08fa41ffbe5690cfab230dd86113f
Mausy5043 <mausy5043@users.noreply.github.com>
2014-12-29 21:51:58 +0100

    Need to create `~/bin` because `cp` won't.

--- 02-post-boot.sh
+++ 02-post-boot.sh
@@ -2,6 +2,9 @@
 
 # This only gets executed if `/tmp/gitbin.reboot` is absent...
 if [ ! -e ~/bin/.rsyncd.secret ]; then
+  if [ ! -d ~/bin ]; then
+    mkdir ~/bin
+  fi
   sudo mount /mnt/backup
   cp /mnt/backup/rbmain/bin/* ~/bin/
   sudo umount /mnt/backup
