72fdfc67940fd0e3b094f47c1b549e0eeceabd93
Mausy5043 <mausy5043@users.noreply.github.com>
2015-01-03 18:02:40 +0100

    Updating install

--- rbian/add-packages.sh
+++ rbian/add-packages.sh
@@ -1,3 +1,7 @@
 echo "Installing extra packages..."
-sudo apt-get -yuV install cups cups-pdf cups-driver-gutenprint hplip-cups 
+sudo apt-get -yuV install cups cups-pdf cups-driver-gutenprint hplip-cups
+echo "Pausing..."
+sleep 60
 sudo apt-get -yuV install openprinting-ppds python-cups python-daemon python-pkg-resources
+echo "Done..."
+sleep 60
