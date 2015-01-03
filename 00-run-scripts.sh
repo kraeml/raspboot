4ec07655f5c60a8e6d94a4413beacea242e8a257
Mausy5043 <mausy5043@users.noreply.github.com>
2015-01-03 17:59:24 +0100

    Run post-boot after bootmail
    Get a mail as soon as possible.

--- 00-run-scripts.sh
+++ 00-run-scripts.sh
@@ -12,10 +12,10 @@ pushd /home/pi/gitbin
 # Boot detection
 if [ ! -e /tmp/gitbin.reboot ]; then
   whoami > /tmp/gitbin.reboot
-  ./01-post-boot.sh | tee -a $logfile | logger -p local7.info -t 01-post-boot
   if [ -e /home/pi/bin/bootmail.py ]; then
     /home/pi/bin/bootmail.py
   fi
+  ./01-post-boot.sh | tee -a $logfile | logger -p local7.info -t 01-post-boot
 fi
 
 # Check for new/updated scripts
