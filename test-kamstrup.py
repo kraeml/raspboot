73ea576038554d1f2f9de9d9cc3cbdad2fb0d6e2
Mausy5043 <mausy5043@users.noreply.github.com>
2015-01-06 18:54:22 +0100

    clean-up

--- test-kamstrup.py
+++ test-kamstrup.py
@@ -39,10 +39,13 @@ def gettelegram():
     except:
       # read error, terminate prematurely
       abort = 2
+
     if line == "!":
       abort = 1
+
     if line != "":
        telegram.append(line)
+
     loops2go = loops2go - 1
     if loops2go < 0:
       abort = 3
@@ -90,66 +93,22 @@ def getelectrastring ():
       if (line[0] == '1-0:1.8.2'):
         electra2in = int(float(line[1]) * 1000)
 
-      #['1-0:2.8.1', '00000.000', 'kWh', '']
-      #if (line[0] == '1-0:2.8.1'):
-      #  electra1out = int(float(line[1]) * 1000)
-
-      #['1-0:2.8.2', '00000.000', 'kWh', '']
-      #if (line[0] == '1-0:2.8.2'):
-      #  electra2out = int(float(line[1]) * 1000)
-
-      #['0-0:96.14.0', '0002', '']
-      #if (line[0] == '0-0:96.14.0'):
-      #  tarif = int(line[1])
-
       #['1-0:1.7.0', '0000.32', 'kW', '']
       if (line[0] == '1-0:1.7.0'):
         powerin = int(float(line[1]) * 1000)
 
-      #['1-0:2.7.0', '0000.00', 'kW', '']
-      #if (line[0] == '1-0:2.7.0'):
-      #  powerout = int(float(line[1]) * 1000)
-
-      #['0-0:17.0.0', '999', 'A', '']
-         # not recorded
-
-      #['0-0:96.3.10', '1', '']
-      #if (line[0] == '0-0:96.3.10'):
-      #  swits = int(line[1])
-
-      #['0-0:96.13.1', '', '']
-         # not recorded
-
-      #['0-0:96.13.0', '', '']
-         # not recorded
 
   out = '{0}, {1}, {2}, {3}'.format(status, electra1in, electra2in, powerin)
   return (out)
 
 if __name__ == "__main__":
-
-  # Everybody! Remember where we parked! -Capt. Kirk
-  # orig_stdout = sys.stdout
-
-  # Define output file
-  #f = file('~/test-electra.csv', 'a')
-  #Redefine system output to our file
-  #sys.stdout = f
   for cnt in range(0, 120):
     time.sleep(8)
     secs = int(commands.getoutput("date +%S"))
+
     if (secs > 15):
       outElectra = getelectrastring()
-
       # Get the time and date in human-readable form...
       outDate = commands.getoutput("date '+%F %H:%M:%S'")
-      # ... and machine-readable form (UNIX-epoch)
-
       # Print the data
       print '{0}, {1}'.format(outDate, outElectra)
-
-  # CLose the file
-  #f.close()
-
-  # Re-set the stdout
-  #sys.stdout = orig_stdout
