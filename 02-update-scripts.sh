ec07863358dbc634c7fb05cb6a5dfd9856b93f88
Mausy5043 <mausy5043@users.noreply.github.com>
2015-01-13 20:49:42 +0100

    sync with dev iso master

--- 02-update-scripts.sh
+++ 02-update-scripts.sh
@@ -5,7 +5,7 @@ minute=$(date "+%M")
 if [ $minute -eq "42" ]; then
   # Update the git scripts once every hour
   # All output is logged by `logger`
-  git fetch origin && git reset --hard origin/master && git clean -f -d
+  git fetch origin && git reset --hard origin/dev && git clean -f -d
   # Log current status
   #git status
   # Set permissions
