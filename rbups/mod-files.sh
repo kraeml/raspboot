5474b0bab1a09e59241287b94debbe34a2eab4e3
Mausy5043 <mausy5043@users.noreply.github.com>
2015-01-14 17:46:10 +0100

    Added CUPS support to `rbups`

--- rbups/mod-files.sh
+++ rbups/mod-files.sh
@@ -1,2 +1,10 @@
 echo "Set permissions... [nut]"
 sudo chmod 0640 /etc/nut/*
+
+echo "Modifying installation..."
+# Add user `pi` to the cups-related groups
+sudo usermod -a -G lp,lpadmin pi
+# mount /var/spool on tmpfs
+echo "tmpfs /var/spool/cups tmpfs   defaults,noatime,size=100M,mode=0755         0 0" | sudo tee -a /etc/fstab >/dev/null
+
+# sudo service cups restart
