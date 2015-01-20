7595ecdb830a7e05e2ce2af39cca76f124e94652
Mausy5043 <mausy5043@users.noreply.github.com>
2015-01-20 21:16:18 +0100

    easy switching

--- 02-update-scripts.sh
+++ 02-update-scripts.sh
@@ -4,10 +4,10 @@ minute=$(date "+%M")
 
 if [ $minute -eq "42" ]; then
   # Update the git scripts once every hour
-  # All output is logged by `logger`
+  
+  #git fetch origin && git reset --hard origin/master && git clean -f -d
   git fetch origin && git reset --hard origin/dev && git clean -f -d
-  # Log current status
-  #git status
+
   python -m compileall .
   # Set permissions
   chmod -R 744 *
