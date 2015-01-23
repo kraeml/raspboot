0a8b0adfc7ae5fdf2a2dceb90404d5217e2a7b81
Mausy5043 <mausy5043@users.noreply.github.com>
2015-01-23 20:59:35 +0100

    Add to do

--- rbian/mod-files.sh
+++ rbian/mod-files.sh
@@ -3,3 +3,15 @@ echo "Modifying installation..."
 # Move `cron` logging to a separate file
 sudo sed -i 's/auth\,authpriv\.none/cron\,auth\,authpriv\.none/' /etc/rsyslog.conf
 sudo sed -i 's/\#cron/cron/' /etc/rsyslog.conf
+
+#/etc/wpa_supplicant/wpa_supplicant.conf
+# + ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+# + update_config=1
+# +
+# + network={
+# +  ssid="..."
+# +  psk="..."
+# +  key_mgmt=WPA-PSK
+# +}
+
+# http://raspberrypi.stackexchange.com/questions/15153/i-get-wifi-timouts-with-the-rt2800usb-driver
