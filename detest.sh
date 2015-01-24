afa90762c29c055b5fb9c7f2a16a636737d8f658
Mausy5043 <mausy5043@users.noreply.github.com>
2015-01-24 14:23:35 +0100

    Add detest.sh
    Used to let the test-environment go live.

---
+++ detest.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# This script helps move the installation from "test" mode to "live" mode
+
+sed -i 's/\#\.\/99-/\.\/99-/' /rootfs/$USRHOME/gitbin/00-run-scripts.sh
+pause 120
+sed -i 's/\#\.\/02-/\.\/02-/' /rootfs/$USRHOME/gitbin/00-run-scripts.sh
+echo
+echo "*** Now live..."
+echo
+echo " Consider activating the nightly backups using crontab -e"
+echo
