9b927454c7734b4c82388192bac17a0dd62611bb
Mausy5043 <mausy5043@users.noreply.github.com>
2015-01-24 14:51:33 +0100

    Correction for path

--- detest.sh
+++ detest.sh
@@ -2,9 +2,9 @@
 
 # This script helps move the installation from "test" mode to "live" mode
 
-sed -i 's/\#\.\/99-/\.\/99-/' /rootfs/$USRHOME/gitbin/00-run-scripts.sh
+sed -i 's/\#\.\/99-/\.\/99-/' /home/pi/gitbin/00-run-scripts.sh
 sleep 120
-sed -i 's/\#\.\/02-/\.\/02-/' /rootfs/$USRHOME/gitbin/00-run-scripts.sh
+sed -i 's/\#\.\/02-/\.\/02-/' /home/pi/gitbin/00-run-scripts.sh
 echo
 echo "*** Now live..."
 echo
