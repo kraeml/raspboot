73a05982792d0d41c210fb46dcd5cbcc81492a43
Mausy5043 <mausy5043@users.noreply.github.com>
2015-01-24 14:32:56 +0100

    update add-packages

--- rbian/add-packages.sh
+++ rbian/add-packages.sh
@@ -1,5 +1,3 @@
 echo "Installing weather monitor packages..."
 #sudo apt-get -yuV install firmware-ralink
-sudo apt-get -yuV install iw
-sudo apt-get -yuV install wireless-tools
-sudo apt-get -yuV install wpasupplicant
+sudo apt-get -yuV install iw wireless-tools wpasupplicant
