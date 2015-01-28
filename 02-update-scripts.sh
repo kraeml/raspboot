73cc6f3700bc06e8f7fae4bfbf7b9c6175715593
Mausy5043 <mausy5043@users.noreply.github.com>
2015-01-28 18:44:28 +0100

    Make sure to stay on `master` branch

--- 02-update-scripts.sh
+++ 02-update-scripts.sh
@@ -5,8 +5,8 @@ minute=$(date "+%M")
 if [ $minute -eq "42" ]; then
   # Update the git scripts once every hour
 
-  #git fetch origin && git reset --hard origin/master && git clean -f -d
-  git fetch origin && git reset --hard origin/dev && git clean -f -d
+  git fetch origin && git reset --hard origin/master && git clean -f -d
+  #git fetch origin && git reset --hard origin/dev && git clean -f -d
 
   python -m compileall .
   # Set permissions
