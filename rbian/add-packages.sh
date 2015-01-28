fbe46de4a9fa76fc09eb021dfaea76373469f0a6
Mausy5043 <mausy5043@users.noreply.github.com>
2015-01-28 21:55:48 +0100

    Add Arduino support

--- rbian/add-packages.sh
+++ rbian/add-packages.sh
@@ -1,4 +1,4 @@
 echo "Installing weather monitor packages..."
 sudo apt-get -yuV install iw wireless-tools wpasupplicant
-
-sudo apt-get -yuV install arduino arduino-mk
+echo "Install Arduino support..."
+sudo apt-get -yuV install arduino arduino-mk make
