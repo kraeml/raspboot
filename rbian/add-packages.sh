2911695aab10a2812ae9aea5eca42ba917b5bd4b
Mausy5043 <mausy5043@users.noreply.github.com>
2015-01-31 15:14:37 +0100

    extra packages for adruino

--- rbian/add-packages.sh
+++ rbian/add-packages.sh
@@ -2,3 +2,4 @@ echo "Installing weather monitor packages..."
 sudo apt-get -yuV install iw wireless-tools wpasupplicant
 echo "Install Arduino support..."
 sudo apt-get -yuV install arduino arduino-mk make
+sudo apt-get install picocom python-dev python-setuptools
