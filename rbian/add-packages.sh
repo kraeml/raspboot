21c385a63faef7cf10e4082da5bec3beb572c5e4
Mausy5043 <mausy5043@users.noreply.github.com>
2015-02-03 18:26:53 +0100

    modified packages

--- rbian/add-packages.sh
+++ rbian/add-packages.sh
@@ -1,5 +1,6 @@
-echo "Installing weather monitor packages..."
+echo "Installing WIFI support..."
 sudo apt-get -yuV install iw wireless-tools wpasupplicant
 echo "Install Arduino support..."
-sudo apt-get -yuV install arduino arduino-mk make
+sudo apt-get -yuV install arduino make
+#arduino-mk
 sudo apt-get install picocom python-dev python-setuptools python-serial
