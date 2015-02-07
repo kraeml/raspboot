921d0834282888f55b960ee87c27d343dabf306b
Mausy5043 <mausy5043@users.noreply.github.com>
2015-02-07 10:21:31 +0100

    3

--- testserial.py
+++ testserial.py
@@ -17,52 +17,29 @@ def gettelegram(cmd):
       # read error, terminate prematurely
       abort = 2
 
-    if line[0] == cmd:
-      if line [len(line) - 1] == "!"
-        telegram = line[1]
-        abort = 1
-
-    #if line != "":
-    #   telegram = line
-    #   abort = 1
+    if line != "":
+      line = line.strip().split()
+      if line[0] == cmd:
+        if line[-1] == "!":
+          telegram = line[-2]
+          abort = 1
 
     loops2go = loops2go - 1
     if loops2go < 0:
       abort = 3
 
-  # test for correct start of telegram
-  #if telegram[0][0] != "/":
-  #  abort = 2
-
-  #try:
-  #  serial.XOFF
-  #  port.flush()
-  #  port.close()
-  #except:
-  #  abort == 5
-
   # Return codes:
   # abort == 1 indicates a successful read
-  # abort == 2 means that no valid data was read from the serial port
-  # abort == 3 indicates a data overrun. More lines were received than expected.
-  # abort == 4 indicates a serial port open error.
-  # abort == 5 indicates a serial port close error.
+  # abort == 2 means that a serial port read/write error occurred
+  # abort == 3 no valid data after several attempts
+
   return (telegram, abort)
 
 if __name__ == "__main__":
   telegram, status = gettelegram("V")
   dt = commands.getoutput("date '+%F %H:%M:%S'")
 
-  #port.write("v")
-  # get data
-  #line = port.readline()
-  #port.flush()
-  #discard the first line.
-  #line = port.readline()
-  #port.flush()
-  #port.close()
-
-  #f = file('/tmp/testser.txt', 'a')
-  #f.write('{0}, {1}'.format(dt, line))
+  f = file('/tmp/testser.txt', 'a')
+  f.write('{0}, {1}'.format(dt, line))
   print '{0}, {1}'.format(dt, telegram)
-  #f.close()
+  f.close()
