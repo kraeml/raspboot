e14487bffeb7478ef93e278033a251859fc5b880
Mausy5043 <mausy5043@users.noreply.github.com>
2015-02-07 09:28:13 +0100

    new telegram style support

--- testserial.py
+++ testserial.py
@@ -1,36 +1,83 @@
 #!/usr/bin/python
 import serial, commands, sys
-#var = 1
-#line = "empty"
-#ser = serial.Serial('/dev/ttyACM0', 9600, timeout=100)
-#ser.close()
 
-#ser.open()
+port = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
 
-#while (var == 1):
-#  f = open('/tmp/workfile', 'a')
-#  line = ser.readline()
-#  print line
-#  f.write(line)
-#  ser.flush()
-#  f.close()
+def gettelegram(cmd):
+  # flag used to exit the while-loop
+  abort = 0
+  # countdown counter used to prevent infinite loops
+  loops2go = 40
+  # storage space for the telegram
+  telegram = []
+  # end of line delimiter
+  delim = "\x0a"
 
-#ser.close()
+  #try:
+  #  port.open()
+  #  serial.XON
+  #except:
+  #  abort == 4
+  #  # open error terminates immediately
+  #  return telegram, abort
 
-#get date/time
-dt = commands.getoutput("date '+%F %H:%M:%S'")
+  while abort == 0:
+    try:
+      port.flush()
+      port.write(cmd)
+      line = port.readline()
+      # this doesn't seem to work
+      #line = str(port.readline()).strip()
+      #line = "".join(iter(lambda:port.read(1),delim)).strip()
+    except:
+      # read error, terminate prematurely
+      abort = 2
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
+    #if line[0] == "!":
+    #  abort = 1
+    telegram = line
+    abort =1
 
-# get data
-line = ser.readline()
-ser.flush()
-#discard the first line.
-line = ser.readline()
-ser.flush()
-ser.close()
+    #if line != "":
+    #   telegram.append(line)
 
-f = file('/tmp/testser.txt', 'a')
-f.write('{0}, {1}'.format(dt, line))
-#print '{0}, {1}'.format(dt, line)
-f.close()
+    #loops2go = loops2go - 1
+    #if loops2go < 0:
+    #  abort = 3
+
+  # test for correct start of telegram
+  #if telegram[0][0] != "/":
+  #  abort = 2
+
+  #try:
+  #  serial.XOFF
+  #  port.flush()
+  #  port.close()
+  #except:
+  #  abort == 5
+
+  # Return codes:
+  # abort == 1 indicates a successful read
+  # abort == 2 means that no valid data was read from the serial port
+  # abort == 3 indicates a data overrun. More lines were received than expected.
+  # abort == 4 indicates a serial port open error.
+  # abort == 5 indicates a serial port close error.
+  return (telegram, abort)
+
+if __name__ == "__main__":
+  telegram, status = gettelegram("v")
+  dt = commands.getoutput("date '+%F %H:%M:%S'")
+
+  #port.write("v")
+  # get data
+  #line = port.readline()
+  #port.flush()
+  #discard the first line.
+  #line = port.readline()
+  #port.flush()
+  #port.close()
+
+  #f = file('/tmp/testser.txt', 'a')
+  #f.write('{0}, {1}'.format(dt, line))
+  print '{0}, {1}'.format(dt, telegram)
+  #f.close()
