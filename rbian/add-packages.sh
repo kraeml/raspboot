0efb6762d4caa4ecc14e0d74301de8ce7c967c31
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-02-09 17:37:21 +0100

    Add additional packages required and d/l git package

--- rbian/add-packages.sh
+++ rbian/add-packages.sh
@@ -1,6 +1,21 @@
 echo "Installing WIFI support..."
 sudo apt-get -yuV install iw wireless-tools wpasupplicant
-echo "Install Arduino support..."
-sudo apt-get -yuV install arduino make
-#arduino-mk
-sudo apt-get install picocom python-dev python-setuptools python-serial
+
+echo "Installing Arduino support..."
+sudo apt-get -yuV install arduino
+
+# make
+# arduino-mk
+echo "Installing Serial Port package..."
+sudo apt-get -yuV install picocom
+echo "Installing additional Python packages..."
+sudo apt-get -yuV python-dev python-setuptools python-serial
+sudo apt-get -yuV install python-matplotlib
+
+# Retrieve and install Arduino support files
+echo "Installing Arduino support..."
+git clone https://github.com/Mausy5043/arduino.git
+chmod -R 744 arduino/
+pushd arduino/ino
+  python setup.py
+popd
