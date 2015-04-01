37298c9bc1f129b1fca4c3b514d4980b8264ebe8
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-04-01 18:31:20 +0200

    output correlation coefs to file

--- testgraphs.py
+++ testgraphs.py
@@ -6,7 +6,7 @@ from matplotlib.dates import strpdate2num
 import numpy as np
 import pylab as pl
 
-import os, time, headstails
+import os, time, headstails, commands
 
 os.nice(10)
 
@@ -19,6 +19,28 @@ def taildata():
   for c in F:
     h.write("%s" % (c) )
   h.close()
+
+def corrs():
+  C=np.loadtxt('/tmp/corr.txt',delimiter=',',converters={0:strpdate2num("%Y-%m-%d %H:%M:%S")})
+  # 1 = ATMEGA vs DS18B20
+  # 2 = DHT22 vs. DS18B20
+  # 3 = TMP36 vs. DS18B20
+  #
+  # correlation coefficients a,b : f(x) =  a*x + b
+  # r2 : R^2 of data to correlation coefs
+  # 1,2,3 = a  (slope)
+  # 4,5,6 = b  (offset)
+  # 6,7,8 = r2 (R^2)
+
+  A1 = C[:,1]
+  A2 = C[:,2]
+  A3 = C[:,3]
+  B1 = C[:,4]
+  B2 = C[:,5]
+  B3 = C[:,6]
+  R1 = C[:,7]
+  R2 = C[:,8]
+  R3 = C[:,9]
 
 def graphs():
   C=np.loadtxt('/tmp/taildata.txt',delimiter=',',converters={0:strpdate2num("%Y-%m-%d %H:%M:%S")})
@@ -52,36 +74,44 @@ def graphs():
   ab = np.polyfit(A1,A2,1)
   fit = np.poly1d(ab)
   r2 = np.corrcoef(A1,A2)[0,1]
+  print "ATMEGA vs. DS18B20"
+  a1 = ab[0]
+  b1 = ab[1]
+  r21 = r2
+  print a1, b1, r21
+  print ""
   pl.plot(A1,A2,'r.', label='ATMEGA vs. DS18B20', alpha=0.7)
   pl.plot(A1_extrema,fit(A1_extrema),'c-')
   pl.annotate('{0}'.format(r2) , xy=(min(A1)+0.5,fit(min(A1))), size=6, color='c' )
-  print "ATMEGA vs. DS18B20"
-  print ab
-  print r2
-  print ""
 
   ab = np.polyfit(A3,A2,1)
   fit = np.poly1d(ab)
   r2 = np.corrcoef(A3,A2)[0,1]
+  print "DHT22 vs. DS18B20"
+  a2 = ab[0]
+  b2 = ab[1]
+  r22 = r2
+  print a2, b2, r22
+  print ""
   pl.plot(A3,A2,'g.', label='DHT22  vs. DB18B20', alpha=0.7)
   pl.plot(A3_extrema,fit(A3_extrema),'m-')
   pl.annotate('{0}'.format(r2) , xy=(min(A3)+0.5,fit(min(A3))), size=6, color='m' )
-  print "DHT22 vs. DS18B20"
-  print ab
-  print r2
-  print ""
 
   ab = np.polyfit(A9,A2,1)
   fit = np.poly1d(ab)
   r2 = np.corrcoef(A9,A2)[0,1]
+  print "TMP36 vs. DS18B20"
+  a3 = ab[0]
+  b3 = ab[1]
+  r23 = r2
+  print a3, b3, r23
+  print ""
   pl.plot(A9,A2,'b.', label='TMP36  vs. DS18B20', alpha=0.7)
   pl.plot(A9_extrema,fit(A9_extrema),'y-')
   pl.annotate('{0}'.format(r2) , xy=(min(A9)+0.5,fit(min(A9))), size=6, color='y' )
-  print "TMP36 vs. DS18B20"
-  print ab
-  print r2
-  print ""
 
+  print "Sensor correlations graph"
+  print ""
   pl.title('Sensor correlations')
   pl.xlabel("T(x) [degC]")
   pl.ylabel("T(y),DS18B20 [degC]")
@@ -111,6 +141,8 @@ def graphs():
   D = matplotlib.dates.num2date(C[:,0])
 
   pl.close()
+  print "Temperature trends"
+  print ""
   pl.plot(D,A1, '.r', label='ATMEGA')
   pl.plot(D,A2, '.y', label='DS18B20')
   pl.plot(D,A3, '.g', label='DHT22')
@@ -123,6 +155,8 @@ def graphs():
   pl.savefig('/tmp/D123.png')
 
   pl.close()
+  print "Relative humidity trend"
+  print ""
   pl.plot(D,A4,'.b')
   pl.title('Relative humidity trend')
   pl.ylabel('RH [%]')
@@ -131,6 +165,8 @@ def graphs():
   pl.savefig('/tmp/D4.png')
 
   pl.close()
+  print "Dewpoint trends"
+  print""
   pl.plot(D,A5,'.r', label='DP1')
   pl.plot(D,A6,'.b', label='DP2')
   pl.title('Dewpoint trends')
@@ -141,6 +177,8 @@ def graphs():
   pl.savefig('/tmp/D56.png')
 
   pl.close()
+  print "Heat Index trends"
+  print ""
   pl.plot(D,A7,'.b')
   pl.title('Heat Index trend')
   pl.ylabel('T [degC]')
@@ -149,12 +187,19 @@ def graphs():
   pl.savefig('/tmp/D7.png')
 
   pl.close()
+  print "Solar charger trend"
+  print""
   pl.plot(D,A8,'.b')
   pl.title('Solar charge trend')
   pl.ylabel('Charge [V]')
   pl.grid(True)
   pl.gcf().autofmt_xdate()
   pl.savefig('/tmp/D8.png')
+
+  f = file('/tmp/corr.txt', 'a')
+  od = commands.getoutput("date '+%F %H:%M:%S'")
+  f.write('{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}\n'.format(od, a1, a2, a3, b1, b2, b3, r21, r22, r23))
+  f.close()
 
   return
 
