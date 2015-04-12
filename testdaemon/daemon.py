bb1e2073ae4a3935d792734afb5aa76e2a4819fd
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-04-12 13:14:30 +0200

    prime function added for testing

--- testdaemon/daemon.py
+++ testdaemon/daemon.py
@@ -6,17 +6,23 @@
 
 # Adapted by M.Hendrix
 
-import sys, time
+import sys, datetime, time, math
 from libdaemon import Daemon
 
 class MyDaemon(Daemon):
 	def run(self):
 		cnt=0
+		limit=100000
 		while True:
+			startTime=datetime.datetime.now()
+			# Measure and print the elapsed time
+			count = find_primes(limit)
+			elapsed=datetime.datetime.now()-startTime
 			f=file('/tmp/testd','a')
-			f.write('{0}\n'.format(cnt))
+			f.write('{0}, {1}\n'.format(cnt, elapsed, count))
 			f.close
 			cnt = cnt + 1
+			limit=limit+100
 			time.sleep(10)
 
 if __name__ == "__main__":
@@ -39,3 +45,21 @@ if __name__ == "__main__":
 	else:
 		print "usage: %s start|stop|restart|foreground" % sys.argv[0]
 		sys.exit(2)
+
+# Function to search for prime numbers
+# within number range
+def find_primes(upper_limit):
+  count = 0
+  candidate = 3
+  while(candidate <= upper_limit):
+    trial_divisor = 2
+    prime = 1 # assume it's prime
+    while(trial_divisor**2 <= candidate and prime):
+      if(candidate%trial_divisor == 0):
+        prime = 0 # it isn't prime
+      trial_divisor+=1
+    if(prime):
+      #print_prime(candidate)
+      count += 1
+    candidate += 2
+  return count
