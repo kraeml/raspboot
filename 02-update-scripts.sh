b2ce5f9b041374c2c053aa468c3ce2bb3c9f73c9
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-04-12 17:16:23 +0200

    20150412.1716

--- 02-update-scripts.sh
+++ 02-update-scripts.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
 #git fetch origin && git reset --hard origin/master && git clean -f -d
-git fetch origin
-DIFFdmn=$(git --no-pager diff --name-only dev..origin/dev -- ./testdaemon/daemon.py)
-DIFFlib=$(git --no-pager diff --name-only dev..origin/dev -- ./testdaemon/libdaemon.py)
-
+git fetch origin && \
+DIFFdmn=$(git --no-pager diff --name-only dev..origin/dev -- ./testdaemon/daemon.py) && \
+DIFFlib=$(git --no-pager diff --name-only dev..origin/dev -- ./testdaemon/libdaemon.py) && \
 git reset --hard origin/dev && git clean -f -d
 
 python -m compileall .
@@ -12,12 +11,12 @@ python -m compileall .
 chmod -R 744 *
 
 if [[ -n "$DIFFdmn" ]]; then
-    logger "daemon has changed"
+    logger -t 02-update-scripts "daemon has changed"
     ./testdaemon/daemon.py restart
 fi
 
 if [[ -n "$DIFFlib" ]]; then
-    logger "daemonlib has changed"
+    logger -t 02-update-scripts "daemonlib has changed"
     ./testdaemon/daemon.py stop
     sleep 1
     ./testdaemon/daemon.py start
