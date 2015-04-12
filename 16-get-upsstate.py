84921b9a91ba3c95065ed2c464c2fd52d23204e4
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-04-12 09:59:58 +0200

    clean up indentation

--- 16-get-upsstate.py
+++ 16-get-upsstate.py
@@ -3,35 +3,35 @@
 import sys, commands, time
 
 def main():
-    # Everybody! Remember where we parked! -Capt. Kirk
-    orig_stdout = sys.stdout
+  # Everybody! Remember where we parked! -Capt. Kirk
+  orig_stdout = sys.stdout
 
-    # Define output file
-    f = file('/tmp/16-aux-ups.csv', 'a')
-    # Redefine system output to our file
-    sys.stdout = f
+  # Define output file
+  f = file('/tmp/16-aux-ups.csv', 'a')
+  # Redefine system output to our file
+  sys.stdout = f
 
-    # Get the time and date in human-readable form...
-    outDate = commands.getoutput("date '+%F %H:%M:%S'")
-    # ... and machine-readable form (UNIX-epoch)
-    outUxDate =commands.getoutput("date +%s")
+  # Get the time and date in human-readable form...
+  outDate = commands.getoutput("date '+%F %H:%M:%S'")
+  # ... and machine-readable form (UNIX-epoch)
+  outUxDate =commands.getoutput("date +%s")
 
-    # UPS status
-    outUPSvoltin = commands.getoutput("upsc ups@localhost input.voltage")
-    outUPSvolt = commands.getoutput("upsc ups@localhost battery.voltage")
-    outUPScharge = commands.getoutput("upsc ups@localhost battery.charge")
-    outUPSload = commands.getoutput("upsc ups@localhost ups.load")
-    outUPSrun = commands.getoutput("upsc ups@localhost battery.runtime")
+  # UPS status
+  outUPSvoltin = commands.getoutput("upsc ups@localhost input.voltage")
+  outUPSvolt = commands.getoutput("upsc ups@localhost battery.voltage")
+  outUPScharge = commands.getoutput("upsc ups@localhost battery.charge")
+  outUPSload = commands.getoutput("upsc ups@localhost ups.load")
+  outUPSrun = commands.getoutput("upsc ups@localhost battery.runtime")
 
 
-    # Print the data
-    print '{0}, {1}, {2}, {3}, {4}, {5}, {6}'.format(outDate, outUxDate, outUPSvoltin, outUPSvolt, outUPScharge, outUPSload, outUPSrun)
+  # Print the data
+  print '{0}, {1}, {2}, {3}, {4}, {5}, {6}'.format(outDate, outUxDate, outUPSvoltin, outUPSvolt, outUPScharge, outUPSload, outUPSrun)
 
-    # Close the file
-    f.close()
+  # Close the file
+  f.close()
 
-    # Re-set the stdout
-    sys.stdout = orig_stdout
+  # Re-set the stdout
+  sys.stdout = orig_stdout
 
 if __name__ == "__main__":
-    main()
+  main()
