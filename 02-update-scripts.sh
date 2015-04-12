d3118875521421cc13c53a383e2e9b0308f45b30
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-04-12 16:02:56 +0200

    detect changes to daemon

--- 02-update-scripts.sh
+++ 02-update-scripts.sh
@@ -1,8 +1,20 @@
 #! /bin/bash
 
 #git fetch origin && git reset --hard origin/master && git clean -f -d
-git fetch origin && git reset --hard origin/dev && git clean -f -d
+git fetch origin
+DIFFdmn=$(git --no-pager diff --name-only dev..origin/dev -- ./testdaemon/daemon.py)
+DIFFlib=$(git --no-pager diff --name-only dev..origin/dev -- ./testdaemon/libdaemon.py)
+
+git reset --hard origin/dev && git clean -f -d
 
 python -m compileall .
 # Set permissions
 chmod -R 744 *
+
+if [[ -n "$DIFFdmn" ]]; then
+    logger "daemon has changed"
+fi
+
+if [[ -n "$DIFFlib" ]]; then 
+    logger "daemon has changed"
+fi
