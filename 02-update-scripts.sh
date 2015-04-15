485535bf7e7c6fc7ceee4fb570636c01bda15d2b
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-04-15 18:03:04 +0200

    20150415.1803

--- 02-update-scripts.sh
+++ 02-update-scripts.sh
@@ -1,10 +1,29 @@
 #! /bin/bash
 
-# Update the git scripts once every hour
 
 branch=$(cat ~/bin/gitbin.branch)
 git fetch origin && git reset --hard origin/$branch && git clean -f -d
 
+#git fetch origin && git reset --hard origin/master && git clean -f -d
+#git fetch origin && \
+#DIFFdmn=$(git --no-pager diff --name-only master -- ./testdaemon/daemon.py) && \
+#DIFFdmn=$(git --no-pager diff --name-only dev..origin/dev -- ./testdaemon/daemon.py) && \
+#DIFFlib=$(git --no-pager diff --name-only master -- ./testdaemon/libdaemon.py) && \
+#DIFFlib=$(git --no-pager diff --name-only dev..origin/dev -- ./testdaemon/libdaemon.py) && \
+#git reset --hard origin/dev && git clean -f -d
+
 python -m compileall .
 # Set permissions
 chmod -R 744 *
+
+#if [[ -n "$DIFFdmn" ]]; then
+#    logger -t 02-update-scripts "daemon has changed"
+#    ./testdaemon/daemon.py restart
+#fi
+
+#if [[ -n "$DIFFlib" ]]; then
+#    logger -t 02-update-scripts "daemonlib has changed"
+#    ./testdaemon/daemon.py stop
+#    sleep 1
+#    ./testdaemon/daemon.py start
+#fi
