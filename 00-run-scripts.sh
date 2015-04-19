3e90e8654bee49457aa76af14a396a24433754d4
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-04-19 13:26:18 +0200

    prevent git detecting chmod

--- 00-run-scripts.sh
+++ 00-run-scripts.sh
@@ -13,6 +13,7 @@ pushd /home/pi/gitbin
 if [ ! -e /tmp/gitbin.reboot ]; then
   # Set the flag first to prevent recursive execution
   whoami > /tmp/gitbin.reboot
+  git config core.fileMode false
   ( ./01-post-boot.sh 2>&1 | tee -a ../post-boot.log | logger -p local7.info -t 01-post-boot ) &
 fi
 
