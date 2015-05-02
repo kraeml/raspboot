b50df56a734f35f23a259b59d62e9b28ab052152
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-05-02 16:55:47 +0200

    pylab iso pyplot

--- testgraphs.py
+++ testgraphs.py
@@ -130,26 +130,27 @@ def graphs():
   pl.savefig('/tmp/Dd.png')
 
 
+  pl.close()
+  print "Windroos"
   """
   Demo of bar plot on a polar axis.
   """
-  #import numpy as np
-  import matplotlib.pyplot as plt
 
   N = 20
   theta = np.linspace(0.0, 2 * np.pi, N, endpoint=False)
   radii = 10 * np.random.rand(N)
   width = np.pi / 4 * np.random.rand(N)
 
-  ax = plt.subplot(111, polar=True)
+  ax = pl.subplot(111, polar=True)
   bars = ax.bar(theta, radii, width=width, bottom=0.0)
 
   # Use custom colors and opacity
   for r, bar in zip(radii, bars):
-      bar.set_facecolor(plt.cm.jet(r / 10.))
+      bar.set_facecolor(pl.cm.jet(r / 10.))
       bar.set_alpha(0.5)
 
-  plt.savefig('/tmp/De.png')
+  pl.title('Windroos')
+  pl.savefig('/tmp/De.png')
 
   pl.close()
   print "Temperature trends"
