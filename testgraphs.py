a99e40d6d0103f7619f6b7c6517c4e13a68400e2
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-05-02 17:29:49 +0200

    20150502.1729

--- testgraphs.py
+++ testgraphs.py
@@ -146,7 +146,7 @@ def graphs():
   radii = A13[len(A13)-N-1:len(A13)-1]
   # width of bar
   width = np.pi / 4 * np.random.rand(N)
-  width = np.pi / 4
+  width = np.pi / 8
 
   ax = pl.subplot(111, polar=True)
   ax.set_theta_zero_location("N")
