8747fe3c4c985b73194a029fcd576af164204589
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-06-13 11:30:34 +0200

    various changes for osmc

--- 01-post-boot.sh
+++ 01-post-boot.sh
@@ -28,14 +28,6 @@ fi
 echo "Boot detection mail... "$(date)
 /home/$ME/bin/bootmail.py
 
-# Start daemons, if installed
-# *** change this to /etc/init.d/... files
-#if [ -e ~/raspdiagd ]; then
-#  pushd ~/raspdiagd
-#    ./00-scriptmanager.sh
-#  popd
-#fi
-
 # Additional scripts to be executed on the first boot after install.
 # This makes the `raspbian-ua-netinst` installer more uniform and easier
 # to maintain regardless of the use.
--- osmc/mod-files.sh
+++ osmc/mod-files.sh
@@ -1,22 +1,22 @@
 # `rxbmc` requires more set-up because it is not spawned from raspbian-ua-netinst
 
 echo "Removing AVAHI services we don't offer..."
-sudo rm /etc/avahi/services/ftp.service
-sudo rm /etc/avahi/services/samba.service
+sudo rm /etc/avahi/services/sftp.service
 
 # Create mountpoints
 echo "Creating mountpoints ..."
 sudo mkdir /mnt/backup
 sudo mkdir /mnt/share1
+mkdir etc
 
 # Add mountpoints to /etc/fstab
 echo "Adding mountpoints to /etc/fstab ..."
 echo '# mountpoint for NFS on /media'  | sudo tee --append /etc/fstab
-echo '10.0.1.220:/srv/array1/video /media nfs4 nouser,atime,auto,rw,dev,exec,suid 0   0'  | sudo tee --append /etc/fstab
+echo '10.0.1.220:/srv/array1/video  /media  nfs4  nouser,atime,auto,rw,dev,exec,suid  0 0'  | sudo tee --append /etc/fstab
 echo '# mountpoint for systemlogs and backups'  | sudo tee --append /etc/fstab
-echo '10.0.1.220:/srv/array1/backup /mnt/backup nfs4 nouser,atime,rw,dev,exec,suid,noauto 0   0'  | sudo tee --append /etc/fstab
-echo 'tmpfs	/tmp		tmpfs	nodev,nosuid,size=64M,mode=1777	0	0'  | sudo tee --append /etc/fstab
-echo 'tmpfs	/var/log	tmpfs	defaults,noatime,nosuid,mode=0755,size=96M	0	0'  | sudo tee --append /etc/fstab
+echo '10.0.1.220:/srv/array1/backup /mnt/backup nfs4  nouser,atime,rw,dev,exec,suid,noauto 0 0'  | sudo tee --append /etc/fstab
+echo 'tmpfs /tmp  tmpfs nodev,nosuid,size=64M,mode=1777 0 0'  | sudo tee --append /etc/fstab
+echo 'tmpfs /var/log  tmpfs defaults,noatime,nosuid,mode=0755,size=96M  0 0'  | sudo tee --append /etc/fstab
 
 # Mount networkdisk
 echo "Restoring from backup ..."
@@ -28,21 +28,16 @@ if [ -e ~/.kodi ]; then
   mv ~/.kodi ~/.kodi-org
 fi
 
-echo -n "Restore bin - "
-cp -r /mnt/backup/rxbmc/home/osmc/bin bin
+#echo -n "Restore bin - "
+#cp -r /mnt/backup/osmc/home/osmc/bin/* ~/bin/
 # set flag for raspboot
-echo master > /home/osmc/bin/raspboot.branch
+echo master > /home/osmc/.raspboot.branch
 
 echo -n "Restore etc - "
-cp -r /mnt/backup/rxbmc/home/osmc/etc etc
+cp -r /mnt/backup/osmc/home/osmc/etc/* ~/etc/
 
 # Note: cronjobs may not start until after the reboot!
 sudo crontab -u osmc .crntb
 
 echo -n "Restore invisibles - "
-cp /mnt/backup/rxbmc/home/osmc/.* ~/ 2>/dev/null
-
-hostname rxbmc
-sudo sed -i 's/raspbmc/rxbmc/' /etc/hostname
-sudo sed -i 's/raspbmc/rxbmc/' /etc/hosts
-sudo sed -i 's/raspbmc/rxbmc/' /etc/hosts
+cp /mnt/backup/osmc/home/osmc/.* ~/ 2>/dev/null
