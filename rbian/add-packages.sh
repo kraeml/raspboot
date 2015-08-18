38c2f4961d8d1729a6a6e977354bf132ccc9e714
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-08-18 09:45:34 +0200

    added keyword `install`
    no wonder these packages didn’t get installed ;-)

--- rbian/add-packages.sh
+++ rbian/add-packages.sh
@@ -9,7 +9,7 @@ sudo apt-get -yuV install arduino
 echo "Installing Serial Port package..."
 sudo apt-get -yuV install picocom
 echo "Installing additional Python packages..."
-sudo apt-get -yuV python-dev python-setuptools python-serial
+sudo apt-get -yuV install python-dev python-setuptools python-serial
 sudo apt-get -yuV install python-matplotlib python-bs4 python-lxml
 
 # Retrieve and install Arduino support files
