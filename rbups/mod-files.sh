899b18e1ddd64e8f064fbe4ffebbed75a61c11c2
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-08-27 18:58:10 +0200

    Add mountpoint for storing PDF files

--- rbups/mod-files.sh
+++ rbups/mod-files.sh
@@ -6,6 +6,7 @@ echo "Modifying installation..."
 sudo usermod -a -G lp,lpadmin pi
 # mount /var/spool/cups on tmpfs
 echo "tmpfs /var/spool/cups tmpfs   defaults,noatime,size=100M,mode=0755         0 0" | sudo tee -a /etc/fstab >/dev/null
+echo "//10.0.1.24/storage/USB8GB-0A22-839C/pdf  /media  cifs  rw,guest,uid=1000  0 0" | sudo tee -a /etc/fstab >/dev/null
 
 # Move `cron` logging to a separate file
 sudo sed -i 's/auth\,authpriv\.none/cron\,auth\,authpriv\.none/' /etc/rsyslog.conf
