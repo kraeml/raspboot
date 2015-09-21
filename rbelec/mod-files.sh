321de0f46f1f422b3fb15ae36c7b4b4a38f49e01
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-09-21 17:54:07 +0200

    Modification of /etc/rsyslog no longer needed.
    Alternative /etc/rsyslog is now supplied by mod-rasbian-ua-netinst.

--- rbelec/mod-files.sh
+++ rbelec/mod-files.sh
@@ -1,5 +1,1 @@
 echo "Modifying installation..."
-
-# Move `cron` logging to a separate file
-sudo sed -i 's/auth\,authpriv\.none/cron\,auth\,authpriv\.none/' /etc/rsyslog.conf
-sudo sed -i 's/\#cron/cron/' /etc/rsyslog.conf
--- rbian/mod-files.sh
+++ rbian/mod-files.sh
@@ -1,8 +1,4 @@
 echo "Modifying installation..."
 
-# Move `cron` logging to a separate file
-sudo sed -i 's/auth\,authpriv\.none/cron\,auth\,authpriv\.none/' /etc/rsyslog.conf
-sudo sed -i 's/\#cron/cron/' /etc/rsyslog.conf
-
 # Install information about the wifi-network
 #sudo cp /home/pi/bin/wpa.conf /etc/wpa_supplicant/wpa_supplicant.conf
