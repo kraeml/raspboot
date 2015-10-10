7f0011b6e1b6ba4721458030d72dd55e75c7b635
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-10-10 15:50:07 +0200

    pdf->scans

--- rbups/mod-files.sh
+++ rbups/mod-files.sh
@@ -6,4 +6,4 @@ echo "Modifying installation..."
 sudo usermod -a -G lp,lpadmin pi
 # mount /var/spool/cups on tmpfs
 echo "tmpfs  /var/spool/cups  tmpfs  defaults,noatime,size=100M,mode=0755  0  0" | sudo tee -a /etc/fstab >/dev/null
-echo "//rt-n66u.lan/pdf/      /media  cifs   rw,_netdev,username=scanner,password=scanner,gid=1000,uid=1000  0  0" | sudo tee -a /etc/fstab >/dev/null
+echo "//rt-n66u.lan/scans/      /media  cifs   rw,_netdev,username=scanner,password=scanner,gid=1000,uid=1000  0  0" | sudo tee -a /etc/fstab >/dev/null
