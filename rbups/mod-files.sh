cbe4e9b8932b34f44b758e34fc99f9741be86d4f
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-10-10 15:47:33 +0200

    mountpoint for pdfs

--- rbups/mod-files.sh
+++ rbups/mod-files.sh
@@ -5,5 +5,5 @@ echo "Modifying installation..."
 # Add user `pi` to the cups-related groups
 sudo usermod -a -G lp,lpadmin pi
 # mount /var/spool/cups on tmpfs
-echo "tmpfs /var/spool/cups tmpfs   defaults,noatime,size=100M,mode=0755         0 0" | sudo tee -a /etc/fstab >/dev/null
-echo "//10.0.1.24/storage/USB8GB-0A22-839C/pdf  /media  cifs  rw,_netdev,guest,uid=1000  0 0" | sudo tee -a /etc/fstab >/dev/null
+echo "tmpfs  /var/spool/cups  tmpfs  defaults,noatime,size=100M,mode=0755  0  0" | sudo tee -a /etc/fstab >/dev/null
+echo "//rt-n66u.lan/pdf/      /media  cifs   rw,_netdev,username=scanner,password=scanner,gid=1000,uid=1000  0  0" | sudo tee -a /etc/fstab >/dev/null
