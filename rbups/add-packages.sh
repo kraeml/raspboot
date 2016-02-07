0e0e46ef47adb8d43f0d95d9f6bea4febf9fbda1
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-02-07 10:16:16 +0100

    deprecated CUPS support from `rbups`

--- rbups/add-packages.sh
+++ rbups/add-packages.sh
@@ -1,17 +1,17 @@
 echo "Installing UPS monitor packages..."
 sudo apt-get -yuV install nut-client nut-server
 
-echo "Installing CUPS packages..."
-sudo apt-get -yuV install cups cifs-utils smb-client
-echo "Pausing... (1/4) "
-sleep 60
-sudo apt-get -yuV install hplip-cups openprinting-ppds
-echo "Pausing... (2/4) "
-sleep 60
-sudo apt-get -yuV install cups-pdf cups-driver-gutenprint
-echo "Pausing... (3/4) "
-sleep 60
-sudo apt-get -yuV install python-cups python-daemon python-pkg-resources
+#echo "Installing CUPS packages..."
+#sudo apt-get -yuV install cups cifs-utils smb-client
+#echo "Pausing... (1/4) "
+#sleep 60
+#sudo apt-get -yuV install hplip-cups openprinting-ppds
+#echo "Pausing... (2/4) "
+#sleep 60
+#sudo apt-get -yuV install cups-pdf cups-driver-gutenprint
+#echo "Pausing... (3/4) "
+#sleep 60
+#sudo apt-get -yuV install python-cups python-daemon python-pkg-resources
 sudo apt-get -yuV install python-mysqldb
-echo "Pausing... (4/4)"
-sleep 60
+#echo "Pausing... (4/4)"
+#sleep 60
