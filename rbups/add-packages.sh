c18b7168b2555a6d345a19aa29c97212f8f13743
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-03-03 19:33:49 +0100

    cleaning

--- rbups/add-packages.sh
+++ rbups/add-packages.sh
@@ -1,6 +1,8 @@
 echo "Installing UPS monitor packages..."
 sudo apt-get -yuV install nut-client nut-server
 
+sudo apt-get -yuV install python-mysqldb
+
 #echo "Installing CUPS packages..."
 #sudo apt-get -yuV install cups cifs-utils smb-client
 #echo "Pausing... (1/4) "
@@ -12,6 +14,5 @@ sudo apt-get -yuV install nut-client nut-server
 #echo "Pausing... (3/4) "
 #sleep 60
 #sudo apt-get -yuV install python-cups python-daemon python-pkg-resources
-sudo apt-get -yuV install python-mysqldb
 #echo "Pausing... (4/4)"
 #sleep 60
