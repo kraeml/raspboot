b423d8cf7c70a24dc3708e9abbbe6936f459318a
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-03-03 19:29:15 +0100

    deprecating rxbmc

---
+++ .deprecated/rxbmc/mod-files.sh
@@ -0,0 +1,48 @@
+# `rxbmc` requires more set-up because it is not spawned from raspbian-ua-netinst
+
+echo "Removing AVAHI services we don't offer..."
+sudo rm /etc/avahi/services/ftp.service
+sudo rm /etc/avahi/services/samba.service
+
+# Create mountpoints
+echo "Creating mountpoints ..."
+sudo mkdir /mnt/backup
+sudo mkdir /mnt/share1
+
+# Add mountpoints to /etc/fstab
+echo "Adding mountpoints to /etc/fstab ..."
+echo '# mountpoint for NFS on /media'  | sudo tee --append /etc/fstab
+echo 'boson.lan:/srv/array1/video /media nfs4 nouser,atime,auto,rw,dev,exec,suid 0   0'  | sudo tee --append /etc/fstab
+echo '# mountpoint for systemlogs and backups'  | sudo tee --append /etc/fstab
+echo 'boson.lan:/srv/array1/backup /mnt/backup nfs4 nouser,atime,rw,dev,exec,suid,noauto 0   0'  | sudo tee --append /etc/fstab
+echo 'tmpfs	/tmp		tmpfs	nodev,nosuid,size=64M,mode=1777	0	0'  | sudo tee --append /etc/fstab
+echo 'tmpfs	/var/log	tmpfs	defaults,noatime,nosuid,mode=0755,size=96M	0	0'  | sudo tee --append /etc/fstab
+
+# Mount networkdisk
+echo "Restoring from backup ..."
+echo -n "Mount - "
+sudo mount /mnt/backup
+
+if [ -e ~/.kodi ]; then
+  echo -n "Move existing .kodi folder - "
+  mv ~/.kodi ~/.kodi-org
+fi
+
+echo -n "Restore bin - "
+cp -r /mnt/backup/rxbmc/home/pi/bin bin
+# set flag for raspboot
+echo master > /home/pi/bin/raspboot.branch
+
+echo -n "Restore etc - "
+cp -r /mnt/backup/rxbmc/home/pi/etc etc
+
+# Note: cronjobs may not start until after the reboot!
+sudo crontab -u pi .crntb
+
+echo -n "Restore invisibles - "
+cp /mnt/backup/rxbmc/home/pi/.* ~/ 2>/dev/null
+
+hostname rxbmc
+sudo sed -i 's/raspbmc/rxbmc/' /etc/hostname
+sudo sed -i 's/raspbmc/rxbmc/' /etc/hosts
+sudo sed -i 's/raspbmc/rxbmc/' /etc/hosts
