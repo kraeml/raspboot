3fa59f29b2595a8f85f6e3af738d4234d56df511
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-04-29 12:47:44 +0200

    not loading modules on newer kernel

--- rbagain/mod-files.sh
+++ rbagain/mod-files.sh
@@ -4,8 +4,8 @@ echo "Modifying installation..."
 echo "Installing 1-wire support..."
 # ref: http://domoticx.com/raspberry-pi-temperatuur-sensor-ds18b20-uitlezen/
 # quick check: cat /sys/bus/w1/devices/28-*/w1_slave
-echo "w1-gpio pullup=1"  | sudo tee --append /etc/modules
-echo "w1-therm"          | sudo tee --append /etc/modules
+#echo "w1-gpio pullup=1"  | sudo tee --append /etc/modules
+#echo "w1-therm"          | sudo tee --append /etc/modules
 
 # GPIO ref: https://learn.adafruit.com/adafruits-raspberry-pi-lesson-4-gpio-setup/configuring-gpio
 #           https://www.raspberrypi.org/documentation/usage/gpio/README.md
