b7c230088185213912f435b439ed235a1b9cbcf0
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-04-29 16:28:24 +0200

    change branch

--- rbagain/add-packages.sh
+++ rbagain/add-packages.sh
@@ -6,6 +6,6 @@ sudo apt-get -yuV install build-essential python-dev python-setuptools python-rp
 sudo apt-get -yuV install wiringpi
 gpio readall
 
-sudo apt-get -yuV install python-mysqldb gnuplot gnuplot-nox mysql-client
+sudo apt-get -yuV install gnuplot gnuplot-nox mysql-client python-mysqldb
 
 sudo apt-get -yuV install rpi-update avahi-daemon
