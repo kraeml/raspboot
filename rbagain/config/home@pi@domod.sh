0dc08fcbaa83858923126660de8ba5dbd25119df
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-05-30 16:58:23 +0200

    new tested kernel

--- rbagain/config/home@pi@domod.sh
+++ rbagain/config/home@pi@domod.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# update the kernel to 4.4.8
+# update the kernel
+
+# tested stable commits:
 # Linux rbian 4.4.8+ #880 Fri Apr 22 21:27:42 BST 2016 armv6l GNU/Linux
-# tested stable commit:
 # https://github.com/Hexxeh/rpi-firmware/commit/6d158adcc0cfa03afa17665715706e6e5f0750d2
+#
+# Linux rbagain 4.4.11+ #886 Thu May 19 15:14:34 BST 2016 armv6l GNU/Linux
+# https://github.com/Hexxeh/rpi-firmware/commit/48cfa89779408ecd69db4eb793b846fb7fe40c4b
+
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 echo "% THIS WILL UPDATE THE KERNEL %"
 echo "%       AND INSTALL DOMOD     %"
@@ -11,7 +16,7 @@ echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 echo ""
 
 pushd $HOME
-  sudo rpi-update 6d158adcc0cfa03afa17665715706e6e5f0750d2
+  sudo rpi-update 48cfa89779408ecd69db4eb793b846fb7fe40c4b
 
   sudo sed -i 's/kernel=/#kernel=/' /boot/config.txt
   sudo sed -i 's/initramfs/#initramfs/' /boot/config.txt
