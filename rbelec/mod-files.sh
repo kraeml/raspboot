642e2f774af4ea50a857e803aa55975103651d07
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-08-13 13:11:02 +0200

    update 20160813T1311

--- rbelec/mod-files.sh
+++ rbelec/mod-files.sh
@@ -1,10 +1,8 @@
 echo "Modifying installation..."
-echo "Cloning KAMSTRUPd..."
-pushd $HOME
-  git clone -b master https://github.com/Mausy5043/kamstrupd.git
-  # set permissions
-  chmod -R 0755 $HOME/kamstrupd
-  pushd $HOME/kamstrupd
-    ./install.sh
-  popd
+
+git clone -b master https://github.com/Mausy5043/kamstrupd.git $HOME/kamstrupd
+# set permissions
+chmod -R 0755 $HOME/kamstrupd
+pushd $HOME/kamstrupd
+  ./install.sh
 popd
