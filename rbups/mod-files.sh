08d2e7da169ee2033a1af9ffeb421f1e7f3e6593
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-08-15 09:42:34 +0200

    add upsdiagd

--- rbups/mod-files.sh
+++ rbups/mod-files.sh
@@ -1,9 +1,10 @@
 echo "Set permissions... [nut]"
 sudo chmod 0640 /etc/nut/*
 
-#echo "Modifying installation..."
-# Add user `pi` to the cups-related groups
-#sudo usermod -a -G lp,lpadmin pi
-# mount /var/spool/cups on tmpfs
-#echo "tmpfs  /var/spool/cups  tmpfs  defaults,noatime,size=100M,mode=0755  0  0" | sudo tee -a /etc/fstab >/dev/null
-#echo "//rt-n66u.lan/scans/      /media  cifs   rw,_netdev,username=scanner,password=scanner,gid=1000,uid=1000  0  0" | sudo tee -a /etc/fstab >/dev/null
+echo "UPS monitor installation..."
+git clone -b master https://github.com/Mausy5043/upsdiagd.git $HOME/upsdiagd
+# set permissions
+chmod -R 0755 $HOME/upsdiagd
+pushd $HOME/upsdiagd
+  ./install.sh
+popd
