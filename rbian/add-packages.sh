f888dac8ae2d901a0a10d09bf9eafe585d547d60
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2016-10-29 09:44:35 +0200

    RPF kernel trial

--- rbian/add-packages.sh
+++ rbian/add-packages.sh
@@ -1,3 +1,6 @@
 echo "Installing additional Python packages..."
 #sudo apt-get -yuV install python-dev python-setuptools python-serial
 #sudo apt-get -yuV install python-matplotlib python-bs4 python-lxml
+echo "Switching to the RPF-kernel"
+sudo apt-get -yuV remove raspberrypi-bootloader-nokernel
+sudo apt-get -yuV install raspberrypi-bootloader libraspberrypi0 libraspberrypi-bin
