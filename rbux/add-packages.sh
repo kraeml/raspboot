4de4e88df5ee88bbb46a418594f480a4d5e60cb8
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2016-12-21 17:36:23 +0100

    add samba and FS packages

--- rbux/add-packages.sh
+++ rbux/add-packages.sh
@@ -1,6 +1,23 @@
-echo "Installing additional Python packages..."
-#sudo apt-get -yuV install python-dev python-setuptools python-serial
-#sudo apt-get -yuV install python-matplotlib python-bs4 python-lxml
-#echo "Switching to the RPF-kernel"
-#sudo apt-get -yuV remove raspberrypi-bootloader-nokernel
-#sudo apt-get -yuV install raspberrypi-bootloader libraspberrypi0 libraspberrypi-bin
+
+install_package()
+{
+  # See if packages are installed and install them.
+  package=$1
+  echo "*********************************************************"
+  echo "* Requesting $package"
+  status=$(dpkg-query -W -f='${Status} ${Version}\n' $package 2>/dev/null | wc -l)
+  if [ "$status" -eq 0 ]; then
+    echo "* Installing $package"
+    echo "*********************************************************"
+    sudo apt-get -yuV install $package
+  else
+    echo "* Already installed !!!"
+    echo "*********************************************************"
+  fi
+}
+
+echo "Installing additional packages..."
+install_package "samba"
+install_package "samba-common-bin"
+install_package "btrfs-tools"
+install_package "f2fs-tools"
