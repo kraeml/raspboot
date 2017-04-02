1fb04575367a8e867b93c234be4bdef828a3ba04
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2017-04-02 16:25:29 +0200

    always get the default branch

--- rbups/mod-files.sh
+++ rbups/mod-files.sh
@@ -2,7 +2,7 @@ echo "Set permissions... [nut]"
 sudo chmod 0640 /etc/nut/*
 
 echo "UPS monitor installation..."
-git clone -b master https://github.com/Mausy5043/upsdiagd.git $HOME/upsdiagd
+git clone https://github.com/Mausy5043/upsdiagd.git $HOME/upsdiagd
 # set permissions
 chmod -R 0755 $HOME/upsdiagd
 pushd $HOME/upsdiagd
