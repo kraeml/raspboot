b73af14c3ddb9bde158054f696ef7bcd59e769ed
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-04-22 19:00:07 +0200

    setup Deluge

--- rbtor/mod-files.sh
+++ rbtor/mod-files.sh
@@ -28,3 +28,26 @@ deluged
 sudo pkill deluged
 rm -rf ~/.config/deluge
 ln -s /mnt/icybox/.config/deluge/ ~/.config/deluge
+
+# ref: http://dev.deluge-torrent.org/wiki/UserGuide/Service/systemd
+sudo adduser --system  --gecos "Deluge Service" --disabled-password --group --home /var/lib/deluge deluge
+sudo adduser pi deluge
+
+sudo /etc/init.d/deluged stop
+sudo rm /etc/init.d/deluged
+sudo update-rc.d deluged remove
+
+systemctl enable /etc/systemd/system/deluged.service
+systemctl start deluged
+systemctl status deluged
+
+systemctl enable /etc/systemd/system/deluge-web.service
+systemctl start deluge-web
+systemctl status deluge-web
+
+sudo mkdir -p /mnt/icybox/log/deluge
+sudo chown -R deluge:deluge /mnt/icybox/deluge
+sudo chmod -R 750 /mnt/icybox/deluge
+
+systemctl restart deluged
+systemctl restart deluge-web
