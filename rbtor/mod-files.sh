85976cfa35b7bfd5fe89ebcd330bfe4fa8ab4834
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-04-22 11:54:44 +0200

    add rbtor server

---
+++ rbtor/mod-files.sh
@@ -0,0 +1,25 @@
+echo "Modifying installation..."
+
+echo "Modify /boot/config.txt "
+# Scaled CPU frequency
+echo ""                         | sudo tee -a /boot/config.txt
+echo "# Added by raspboot"      | sudo tee -a /boot/config.txt
+echo "arm_freq=950"             | sudo tee -a /boot/config.txt
+echo "core_freq=450"            | sudo tee -a /boot/config.txt
+echo "sdram_freq=450"           | sudo tee -a /boot/config.txt
+echo "over_voltage=6"           | sudo tee -a /boot/config.txt
+echo ""                         | sudo tee -a /boot/config.txt
+echo "arm_freq_min=500"         | sudo tee -a /boot/config.txt
+echo "core_freq_min=250"        | sudo tee -a /boot/config.txt
+echo "sdram_freq_min=400"       | sudo tee -a /boot/config.txt
+echo "over_voltage_min=0"       | sudo tee -a /boot/config.txt
+echo ""                         | sudo tee -a /boot/config.txt
+echo "force_turbo=0"            | sudo tee -a /boot/config.txt
+echo "temp_limit=70"            | sudo tee -a /boot/config.txt
+echo "..[OK]"
+
+echo "Add mountpoint for external HDD "
+sudo mkdir -p /mnt/icybox
+echo "/dev/sda1 /mnt/icybox ext4 defaults  0   2"       | sudo tee -a /etc/fstab
+
+echo "[OK]"
